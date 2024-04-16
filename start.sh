c754b29c00ea09c660977b719daf61b6cd43c025
Loan J <loan.joliveau@numigi.com>
2024-04-16 15:58:48 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -13,13 +13,12 @@ export PASSWORD_SEED=$(cat /app/data/.password_seed)
 if [[ ! -f /app/data/env.sh ]]; then
     cat > /app/data/env.sh << EOF
 # Add custom environment variables in this file
-
+export FILE_UPLOAD="local"
 EOF
 fi
 
 # environment variables that cannot be overriden
 export APP_SITE_URL="$CLOUDRON_APP_ORIGIN"
-export FILE_UPLOAD="mongodb"
 export MONGO_URI="$CLOUDRON_MONGODB_URL"
 export PASSWORD_SEED="$PASSWORD_SEED"
 
@@ -28,7 +27,7 @@ source /app/data/env.sh
 echo "==> Database migration"
 cd /app/code/apps/app && yarn migrate
 
-echo "==> Changing ownership"
+echo "==> Setting permissions"
 chown -R cloudron:cloudron /app/data /run/growi
 
 echo "==> Starting GROWI"
